95d7f926df632980a06ff640f623b5690e17fa76
Valeriu <3ug3n>
2023-01-26 17:53:28 +0100

    Aggiunto i component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  },
+  {
+    path: 'nuova-task',
+    component: FormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
